ed85b4efde485dedda94cd780ea974b3069a88e1
jules200 <muhayejules@gmail.com>
2024-07-06 17:07:01 +0200

    first commit

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,31 +1,30 @@
 
-def main(option):
-    shopping_list = []
-    if option == "1":
-        item = input("Enter new item: ")
-        shopping_list.append(item)
-        return theoptions()
-    elif option == "2":
-        item = input("Enter item to remove: ")
-        shopping_list.remove(item)
-        return theoptions()
-    elif option == "3":        
-        print(f"This is your shopping list: \n {shopping_list}")
-        return theoptions()
-    elif option == "4":
-        return none
-    else:
-        print("Enter the valid Option")
-        return theoptions()
 
 def display_menu():
-    print("Choose the option")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-    option = int(input("Your choice: "))
-    return main(option)
 
-display_menu()
-
+def main(option):
+    shopping_list = []
+    
+    while True:
+        option = int(input("Your choice: "))
+        if option == "1":
+            item = input("Enter new item: ")
+            shopping_list.append(item)
+            return theoptions()
+        elif option == "2":
+            item = input("Enter item to remove: ")
+            shopping_list.remove(item)
+            return theoptions()
+        elif option == "3":        
+            print(f"This is your shopping list: \n {shopping_list}")
+            return theoptions()
+        elif option == "4":
+            return none
+        else:
+            print("Enter the valid Option")
+main()
