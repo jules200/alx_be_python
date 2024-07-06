cd06e1e789e694ec5f0cf6f47f8b35a61b9adb3e
jules200 <muhayejules@gmail.com>
2024-07-06 17:11:26 +0200

    first commit

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -7,24 +7,25 @@ def display_menu():
     print("3. View List")
     print("4. Exit")
 
-def main(option):
+def main():
     shopping_list = []
     
     while True:
-        option = int(input("Your choice: "))
+        display_menu()
+        option = input("Your choice: ")
         if option == "1":
             item = input("Enter new item: ")
             shopping_list.append(item)
-            return theoptions()
+            pass
         elif option == "2":
             item = input("Enter item to remove: ")
             shopping_list.remove(item)
-            return theoptions()
+            pass
         elif option == "3":        
             print(f"This is your shopping list: \n {shopping_list}")
-            return theoptions()
+            pass
         elif option == "4":
-            return none
+            break
         else:
             print("Enter the valid Option")
 main()
