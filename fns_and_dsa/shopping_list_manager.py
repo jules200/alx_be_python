62f6c869b78215dccf15e418b50f776dfb607430
jules200 <muhayejules@gmail.com>
2024-07-06 16:57:18 +0200

    first commit

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,31 @@
+shopping_list = []
+print(shopping_list)
+def shopping(option):
+    if option == "1":
+        item = input("Enter new item: ")
+        shopping_list.append(item)
+        return theoptions()
+    elif option == "2":
+        item = input("Enter item to remove: ")
+        shopping_list.remove(item)
+        return theoptions()
+    elif option == "3":        
+        print(f"This is your shopping list: \n {shopping_list}")
+        return theoptions()
+    elif option == "4":
+        return none
+    else:
+        print("Enter the valid Option")
+        return theoptions()
+
+def theoptions():
+    print("Choose the option")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+    option = input("Your choice: ")
+    return shopping(option)
+
+theoptions()
+
