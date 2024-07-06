0f12b3eedfe25bcb9939ca0dd497daa376b1fc6a
jules200 <muhayejules@gmail.com>
2024-07-06 21:35:32 +0200

    first commit

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,8 +4,8 @@ def display_current_datetime():
     current_date = datetime.datetime.now()
     print(current_date)
     dd = int(input("Enter the number of days to add to the current date: "))
-    future_date = current_date.date() + datetime.timedelta(days=dd)
-    print(f"Future date: {future_date}")
+    calculate_future_date = current_date.date() + datetime.timedelta(days=dd)
+    print(f"Future date: {calculate_future_date}")
     
 
 display_current_datetime()
