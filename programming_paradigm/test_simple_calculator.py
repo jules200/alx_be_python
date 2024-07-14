cd27b27a7dbac618666fb4cb2b1fa59364d99720
jules200 <muhayejules@gmail.com>
2024-07-14 21:49:36 +0200

    first commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestClass(unittest.TestCase):
         self.assertEqual(self.calc.add(1, 2), 3)
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(1, 2), -1)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(1, 2), 3)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(4, 2), 2)
