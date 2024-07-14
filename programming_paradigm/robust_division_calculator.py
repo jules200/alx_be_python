7d2b04a75650ace7abbac408644658a1809ca424
jules200 <muhayejules@gmail.com>
2024-07-14 19:55:49 +0200

    first commit

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator):
         try:
             num = float(numerator)
             den = float(denominator)
-            return print("The result of the division is"+ str(num/den))
+            return print("The result of the division is "+ str(num/den))
         except:
             print("Error: Please enter numeric values only.")
     except:
