4b326467f24827bc875c46be86066e868d589d4e
jules200 <muhayejules@gmail.com>
2024-07-21 20:36:09 +0200

    first commit

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -3,10 +3,10 @@ class Calculator:
     
     @staticmethod
     def add(a, b):
-        ad = a+b
+        ad = a + b
         return f"The sum is: {ad}"
          
     @classmethod
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
-        return f"The product is: {a*b}"
+        return f"The product is: {a * b}"
