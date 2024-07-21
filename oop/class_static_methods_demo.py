167a8236ff19c2d9f705918d84210108fd181166
jules200 <muhayejules@gmail.com>
2024-07-21 20:37:16 +0200

    first commit

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -9,4 +9,4 @@ class Calculator:
     @classmethod
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
-        return f"The product is: {a * b}"
+        return a * b
